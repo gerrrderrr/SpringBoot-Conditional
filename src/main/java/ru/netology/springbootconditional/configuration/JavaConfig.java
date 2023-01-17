744cc1dc16bdd88c13e0dd08ed3cec85ed6533d2
Olga Kozlova <o.o.olga.kozlova@gmail.com>
2023-01-17 13:10:12 +0300

    Initial commit

---
+++ src/main/java/ru/netology/springbootconditional/configuration/JavaConfig.java
@@ -0,0 +1,23 @@
+package ru.netology.springbootconditional.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.netology.springbootconditional.profile.DevProfile;
+import ru.netology.springbootconditional.profile.ProductionProfile;
+import ru.netology.springbootconditional.profile.SystemProfile;
+
+@Configuration
+public class JavaConfig {
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    @Bean
+    public SystemProfile devProfile() {
+        return new DevProfile();
+    }
+
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "false")
+    @Bean
+    public SystemProfile prodProfile() {
+        return new ProductionProfile();
+    }
+}
