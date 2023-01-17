b7b6d6f925d4f46434a79664fd27b7b23cff15cb
Olga Kozlova <o.o.olga.kozlova@gmail.com>
2023-01-17 13:58:46 +0300

    matchIfMissing was added to annotation ConditionalOnProperty for devProfile

--- src/main/java/ru/netology/springbootconditional/configuration/JavaConfig.java
+++ src/main/java/ru/netology/springbootconditional/configuration/JavaConfig.java
@@ -9,7 +9,7 @@ import ru.netology.springbootconditional.profile.SystemProfile;
 
 @Configuration
 public class JavaConfig {
-    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true")
+    @ConditionalOnProperty(value = "netology.profile.dev", havingValue = "true", matchIfMissing = true)
     @Bean
     public SystemProfile devProfile() {
         return new DevProfile();
